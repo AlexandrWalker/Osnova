d9ec8b9ff2361f8fff694c2ad23be5e7ea170776
AlexandrWalker <alexandr.d.walker@gmail.com>
2025-10-23 20:12:03 +0700

    add technique & team blocks

--- src/js/main.js
+++ src/js/main.js
@@ -87,6 +87,49 @@ document.addEventListener('DOMContentLoaded', () => {
       },
       grabCursor: true,
     });
+
+    const techniqueSlider = new Swiper(".technique__slider", {
+      slidesPerGroup: 1,
+      slidesPerView: 1,
+      spaceBetween: 20,
+      loop: true,
+      speed: 600,
+      mousewheel: {
+        forceToAxis: true,
+      },
+      navigation: {
+        prevEl: ".technique-button-prev",
+        nextEl: ".technique-button-next",
+      },
+      grabCursor: true,
+    });
+
+    const teamSlider = new Swiper(".team__slider", {
+      slidesPerGroup: 1,
+      slidesPerView: 'auto',
+      spaceBetween: 10,
+      // centeredSlides: true,
+      // centeredSlidesBounds: true,
+      loop: true,
+      speed: 500,
+      mousewheel: {
+        forceToAxis: true,
+      },
+      navigation: {
+        prevEl: ".team-button-prev",
+        nextEl: ".team-button-next",
+      },
+      breakpoints: {
+        601: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        835: {
+          slidesPerView: 3,
+          spaceBetween: 0,
+        },
+      },
+    });
   }
 
   /**
@@ -278,40 +321,224 @@ document.addEventListener('DOMContentLoaded', () => {
 
   gsap.set('[data-title="split"]', { opacity: 1 });
 
-  document.fonts.ready.then(() => {
-    let containers = gsap.utils.toArray(".container");
+  // document.fonts.ready.then(() => {
+  let containers = gsap.utils.toArray(".container");
 
-    containers.forEach((container) => {
-      let text = container.querySelector('[data-title="split"]');
-      let animation;
+  containers.forEach((container) => {
+    let text = container.querySelector('[data-title="split"]');
+    let animation;
 
-      SplitText.create(text, {
-        type: "words,lines",
-        mask: "lines",
-        linesClass: "line",
-        autoSplit: true,
-        onSplit: (instance) => {
-          console.log("split")
-          return gsap.from(instance.lines, {
-            yPercent: 120,
-            stagger: 0.1,
-            duration: 1,
-            scrollTrigger: {
-              trigger: container,
-              start: "top 90%",
-              end: "bottom top"
-            }
-          });
-        }
-      });
+    SplitText.create(text, {
+      type: "words,lines",
+      mask: "lines",
+      linesClass: "line",
+      autoSplit: true,
+      onSplit: (instance) => {
+        return gsap.from(instance.lines, {
+          yPercent: 120,
+          stagger: 0.1,
+          duration: 1,
+          scrollTrigger: {
+            trigger: container,
+            start: "top 90%",
+            end: "bottom top"
+          }
+        });
+      }
     });
   });
-
-  
+  // });
 
   $(window).on('resize', function () { ScrollTrigger.refresh() });
 
 });
+
+/*=================Скрипт для блока со скролом=====================*/
+const tasks = document.querySelector('.partners');
+
+if (tasks) {
+  var len = $('.partners__item').length;
+  $(window).on('resize load', function () {
+
+    if (window.innerWidth < "767") {
+      scroll = 0;
+      inc = 0.06; // speed down
+      inc2 = 0.06; // speed up
+      scale = 1;
+      var wH = document.documentElement.clientWidth
+
+
+      $(window).on('scroll', function () {
+        // Find the active element
+        var $activeBlock = $('.active');
+        var element = document.querySelector('.active');
+        var h = element.clientHeight / 200;
+        var distanceToTop = $activeBlock.offset().top - $(window).scrollTop();
+        var top = window.pageYOffset;
+
+        // Scroll direction checks
+        if (scroll > top) {
+          // Scrolling up
+          if ($activeBlock.attr('data-index') != 1) {
+            h = h * 200;
+            if (distanceToTop > h) {
+              var $prevBlock = $activeBlock.prev();
+              $activeBlock.removeClass('active');
+              $prevBlock.addClass('active');
+              $($prevBlock).css('transform', 'scale(1)');
+              scale = 0.90; // set initial scale
+            }
+          }
+        } else if (scroll < top) {
+          // Scrolling down
+
+          if (distanceToTop < 200 && $activeBlock.attr('data-index') != len) {
+            var $nextBlock = $activeBlock.next();
+            $activeBlock.removeClass('active');
+            $nextBlock.addClass('active');
+          }
+
+          if ($activeBlock.attr('data-index') == len && distanceToTop <= 0) {
+            var $prevBlock = $activeBlock.prev();
+            $($prevBlock).css('transform', 'scale(0.90)');
+            scale = 0.90;
+          }
+        }
+
+        // Scaling effect
+        if (scroll > top) {
+          // Scrolling up
+          if (distanceToTop > 50) {
+            var $activeBlock = $('.active');
+            var prevCurrentBlock = $($activeBlock).prev();
+
+            scale += inc2 / 0.006; // Increase scale on scroll up
+            scale = Math.min(scale, 1); // Ensure max scale is 1
+
+            $(prevCurrentBlock).css('transform', 'scale(' + Math.max(1, scale) + ')');
+
+            // Adjust opacity of the over block
+            var $overBlock = $(prevCurrentBlock).find('.over');
+            var newOpacity = Math.max(0, 1 - (distanceToTop / h)); // Calculate new opacity
+            $overBlock.css('opacity', newOpacity);
+          }
+        } else if (scroll < top) {
+          // Scrolling down
+          var $activeBlock = $('.active');
+          var prevCurrentBlock = $($activeBlock).prev();
+
+          scale -= inc * 0.06; // Decrease scale on scroll down
+          if ($(prevCurrentBlock).attr('data-index') == 1) {
+            $(prevCurrentBlock).css('transform', 'scale(' + Math.max(0.89, scale) + ')');
+          }
+          if ($(prevCurrentBlock).attr('data-index') == 2) {
+            $(prevCurrentBlock).css('transform', 'scale(' + Math.max(0.92, scale) + ')');
+          }
+
+          // Adjust opacity of the over block
+          var $overBlock = $(prevCurrentBlock).find('.over');
+          var newOpacity = Math.min(0.6, (distanceToTop / h + 0.02)); // Calculate new opacity
+          $overBlock.css('opacity', newOpacity);
+        }
+
+        if (distanceToTop < 0) {
+          var $prevBlock = $activeBlock.prev();
+          $($prevBlock).css('transform', 'scale(0.90)');
+          scale = 0.90;
+        }
+
+        scroll = top; // Update scroll position
+      });
+    } else {
+
+      scroll = 0;
+      inc = 0.006; // speed down
+      inc2 = 0.008; // speed up
+      scale = 1;
+      var wH = document.documentElement.clientWidth
+
+      $(window).on('scroll', function () {
+        // Find the active element
+        var $activeBlock = $('.active');
+        var element = tasks.querySelector('.active');
+        var h = element.clientHeight / 200;
+        var distanceToTop = $activeBlock.offset().top - $(window).scrollTop() - 130;
+        var top = window.pageYOffset;
+
+        // Scroll direction checks
+        if (scroll > top) {
+          // Scrolling up
+          if ($activeBlock.attr('data-index') != 1) {
+            h = h * 200;
+            if (distanceToTop > h) {
+              var $prevBlock = $activeBlock.prev();
+              $activeBlock.removeClass('active');
+              $prevBlock.addClass('active');
+              $($prevBlock).css('transform', 'scale(1)');
+              scale = 0.92; // set initial scale
+            }
+          }
+        } else if (scroll < top) {
+
+          // Scrolling down
+          if (distanceToTop < h && $activeBlock.attr('data-index') != len) {
+            var $nextBlock = $activeBlock.next();
+            $activeBlock.removeClass('active');
+            $nextBlock.addClass('active');
+            if (scale !== 1) {
+              scale = 1; // set to 1 when scrolling down
+            }
+          }
+
+          if ($activeBlock.attr('data-index') == len && distanceToTop <= 0) {
+            var $prevBlock = $activeBlock.prev();
+            $($prevBlock).css('transform', 'scale(0.92)');
+            scale = 0.92;
+          }
+        }
+
+        // Scaling effect
+        if (scroll > top) {
+          // Scrolling up
+          var $activeBlock = $('.active');
+          var prevCurrentBlock = $($activeBlock).prev();
+
+          scale += inc2; // Increase scale on scroll up
+          scale = Math.min(scale, 1); // Ensure max scale is 1
+
+          $(prevCurrentBlock).css('transform', 'scale(' + Math.max(1, scale) + ')');
+
+          // Adjust opacity of the over block
+          var $overBlock = $(prevCurrentBlock).find('.over');
+          var newOpacity = Math.max(0, 1 - (distanceToTop / h)); // Calculate new opacity
+          $overBlock.css('opacity', newOpacity);
+        } else if (scroll < top) {
+          // Scrolling down
+          var $activeBlock = $('.active');
+          var prevCurrentBlock = $($activeBlock).prev();
+
+          scale -= inc; // Decrease scale on scroll down
+
+          $(prevCurrentBlock).css('transform', 'scale(' + Math.max(0.90, scale) + ')');
+
+          // Adjust opacity of the over block
+          var $overBlock = $(prevCurrentBlock).find('.over');
+          var newOpacity = Math.min(0.6, (distanceToTop / h)); // Calculate new opacity
+          $overBlock.css('opacity', newOpacity);
+        }
+
+        if (distanceToTop < 0) {
+          var $prevBlock = $activeBlock.prev();
+          $($prevBlock).css('transform', 'scale(0.92)');
+          scale = 0.92;
+        }
+
+        scroll = top; // Update scroll position
+      });
+
+    }
+  });
+}
 
 function checkCookies() {
   document.cookie = 'COOKIE_ACCEPT=1;path=\'/\';expires:' + (new Date(new Date().getTime() + 86400e3 * 365).toUTCString());
